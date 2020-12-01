834dcd9dbbc128540c40a3346e69adf8af9d30d1
data-baker <hushuaijun@data-baker.com>
2020-12-01 20:02:45 +0800

    调整鉴权细节

--- app/src/main/java/com/baker/synthesizer/AudioTrackPlayerActivity.java
+++ app/src/main/java/com/baker/synthesizer/AudioTrackPlayerActivity.java
@@ -30,7 +30,6 @@ public class AudioTrackPlayerActivity extends AppCompatActivity {
         bakerSynthesizer = new BakerSynthesizer(clientId, clientSecret);
         //私有化部署时，初始化方式
 //        bakerSynthesizer = new BakerSynthesizer();
-
     }
 
     BakerCallback bakerCallback = new BakerCallback() {
@@ -98,7 +97,7 @@ public class AudioTrackPlayerActivity extends AppCompatActivity {
         bakerSynthesizer.setBakerCallback(bakerCallback);
         /**********************以下是选填参数**************************/
         //设置发音人声音名称，默认：标准合成_模仿儿童_果子
-        bakerSynthesizer.setVoice(BakerConstants.VOICE_NORMAL);
+        bakerSynthesizer.setVoice("特色合成_儿童声音_小恐龙");
         //合成请求文本的语言，目前支持ZH(中文和中英混)和ENG(纯英文，中文部分不会合成),默认：ZH
         bakerSynthesizer.setLanguage(BakerConstants.LANGUAGE_ZH);
         //设置播放的语速，在0～9之间（支持浮点值），不传时默认为5
